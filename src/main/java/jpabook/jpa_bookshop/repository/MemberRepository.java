d0ab72025cc4cd028801e7afa7d399ff6024ac66
sanghui <tkdgml719@gamil.com>
2024-11-10 21:39:06 +0900

    회원 도메인 개발 -회원 리포지토리 개발 -회원 서비스 개발

---
+++ src/main/java/jpabook/jpa_bookshop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpa_bookshop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpa_bookshop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository  //컴포넌트 스캔대상이 되어 스프링 빈으로 등록된다.
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    /*@PersistenceUnit : 엔티티 메니저 팩토리 주입*/
+   // @PersistenceContext /JPA EntityManager 주입
+    private final EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
