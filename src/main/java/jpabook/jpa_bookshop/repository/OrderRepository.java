77bbc8d3e5d93995ac01e83c537a0a35ba220412
sanghui <tkdgml719@gamil.com>
2024-11-13 20:22:12 +0900

    웹 계층 개발 -home 화면과 레이아웃

--- src/main/java/jpabook/jpa_bookshop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpa_bookshop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpa_bookshop.repository;
 
 import jakarta.persistence.EntityManager;
 import jpabook.jpa_bookshop.domain.Order;
+import jpabook.jpa_bookshop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -22,9 +23,21 @@ public class OrderRepository {
     public Order findOne(Long id){
         return em.find(Order.class, id);
     }
-
-  /* TODO findAll -> 검색기능 추가
-    public List<Order> findAll(){}*/
-
-
+    
+    
+    //전체 조회-> 검색 기능
+    public List<Order> findAll(OrderSearch orderSearch){
+        //동적 쿼리 필요 
+        //값이 다 있는 경우
+        List<Order> resultList = em.createQuery("select o from Order o join o.member m " +
+                "where o.status = :status and m.name = :name", Order.class)
+                //객체이기 때문에 참조하는 스타일로 조인해야 한다.
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                //.setFirstResult(1)
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+        return resultList;
+        
+    }
 }
