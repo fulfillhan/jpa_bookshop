073ef454a86fca0cc1cccc147a7aa5d053185e94
sanghui <tkdgml719@gamil.com>
2024-11-14 16:02:10 +0900

    웹 계층 개발 -상품 목록과 상품 수정 도메인 모델 패턴개념 정리 추가

---
+++ src/main/java/jpabook/jpa_bookshop/controller/ItemController.java
@@ -0,0 +1,80 @@
+package jpabook.jpa_bookshop.controller;
+
+import jpabook.jpa_bookshop.domain.item.Book;
+import jpabook.jpa_bookshop.domain.item.Item;
+import jpabook.jpa_bookshop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private ItemService itemService;
+
+    /*
+    * 상품 등록
+    * */
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form",new BookForm());
+        return "items/createItemForm";
+    }
+
+    /*
+     * 상품 등록
+     * */
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        Book newBook = Book.createBook(form);
+        itemService.saveItem(newBook);
+        return "redirect:/";
+    }
+
+    /*
+    * 상품목록
+    * */
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "itmes/itemList";
+    }
+
+    /*
+    * 상품 수정
+    * */
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(Model model, @PathVariable Long itemId){
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setAuthor(item.getAuthor());
+        form.setPrice(item.getPrice());
+        form.setIsbn(item.getIsbn());
+        form.setStockQuantity(item.getStockQuantity());
+
+        model.addAttribute("form",form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String update(@PathVariable Long itemId, @ModelAttribute BookForm form){
+        Book book = new Book();
+        book.setId(itemId);
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items"; //목록을 되돌아가기
+    }
+}
--- src/main/java/jpabook/jpa_bookshop/domain/item/Book.java
+++ src/main/java/jpabook/jpa_bookshop/domain/item/Book.java
@@ -2,6 +2,7 @@ package jpabook.jpa_bookshop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jpabook.jpa_bookshop.controller.BookForm;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,4 +14,14 @@ public class Book extends Item {
     private String author;
     private String isbn;
 
+
+    public static Book createBook(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setIsbn(form.getIsbn());
+        book.setAuthor(form.getAuthor());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        return book;
+    }
 }
