ea1cdf78df97b03bd293a3ad157ff5577f69c7ec
sanghui <tkdgml719@gamil.com>
2024-11-23 14:45:22 +0900

    조회 성능 최적화 1. 엔티티 조회의 문제점 2. dto로 변환해서 조회

--- src/main/java/jpabook/jpa_bookshop/JpaBookshopApplication.java
+++ src/main/java/jpabook/jpa_bookshop/JpaBookshopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpa_bookshop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaBookshopApplication {
@@ -10,5 +12,27 @@ public class JpaBookshopApplication {
 		SpringApplication.run(JpaBookshopApplication.class, args);
 
 	}
+
+
+	/*
+	기본적으로 초기화된 프록시만 노출, 초기화 되지 않은 프록시 노출 안함
+	*/
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule(){
+		return new Hibernate5JakartaModule();
+	}
+
+/*
+	 json 생성시 강제로 지연 로딩을 하려면
+
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule(){
+		Hibernate5JakartaModule module = new Hibernate5JakartaModule();
+		//강제로 지연 로딩
+		module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return module;
+	}
+*/
+
 
 }
