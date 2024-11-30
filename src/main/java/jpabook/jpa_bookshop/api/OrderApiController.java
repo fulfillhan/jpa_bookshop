b7f239af741e255d419b376b529c610e6e81f853
sanghui <tkdgml719@gamil.com>
2024-11-30 17:40:56 +0900

    컬렉션 조회 성능 최적화 *jpa에서 dto로 직접 조회 -컬렉션 조회 최적화

--- src/main/java/jpabook/jpa_bookshop/api/OrderApiController.java
+++ src/main/java/jpabook/jpa_bookshop/api/OrderApiController.java
@@ -4,6 +4,8 @@ import jpabook.jpa_bookshop.domain.Order;
 import jpabook.jpa_bookshop.domain.OrderItem;
 import jpabook.jpa_bookshop.domain.OrderSearch;
 import jpabook.jpa_bookshop.repository.OrderRepository;
+import jpabook.jpa_bookshop.repository.order.query.OrderQueryDto;
+import jpabook.jpa_bookshop.repository.order.query.OrderQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,7 @@ import static java.util.stream.Collectors.*;
 @RequiredArgsConstructor
 public class OrderApiController {
         private final OrderRepository orderRepository;
+        private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -44,7 +47,8 @@ public class OrderApiController {
     }
 
     /*
-    * 페지 조인 적용하기
+    엔티티 -> dtp 변경(페치 조인 적용)
+    *
     * 1:n 이면 n 만큼 데이터가 나온다. -> 데이터 뻥튀기 즉, 중복되 row 조회
     *단점 :  페이징 불가!
     * */
@@ -67,4 +71,23 @@ public class OrderApiController {
          List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
          return orders.stream().map(order -> new OrderDto(order)).collect(toList());
     }
+
+    /*
+    * JPA->DTO 로 변환하기
+    * */
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    /*
+     * JPA->DTO 로 변환하기(성능 최적화하기)
+     * */
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+
 }
